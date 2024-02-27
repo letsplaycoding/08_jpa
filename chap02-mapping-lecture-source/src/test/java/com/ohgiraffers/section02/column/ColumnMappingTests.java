7d03373c920e56dd214047398b40592ee0a819ed
alu98@naver.com <alu98@naver.com>
2024-02-27 10:04:00 +0900

    section02 ColumnMappingTests 까지 완료

---
+++ chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section02/column/ColumnMappingTests.java
@@ -0,0 +1,68 @@
+package com.ohgiraffers.section02.column;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ColumnMappingTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void  initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @Test
+    public void 컬럼에서_사용하는_속성_테스트() {
+        com.ohgiraffers.section02.column.Member member = new com.ohgiraffers.section02.column.Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("짱구");
+        member.setPhone("010-1234-9876");
+        member.setEmail("alu9@anevrc.ocm");
+        member.setAddress("서울시 칠팔구");
+        member.setEnrollDate(new java.util.Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+
+        // when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        entityManager.persist(member);
+
+//        entityManager.flush();
+
+        // then
+        com.ohgiraffers.section02.column.Member foundMember = entityManager.find(Member.class, 1);
+        foundMember.setNickname("동해번쩍 서해번짹");
+
+        entityTransaction.commit();
+        assertEquals(member, foundMember);
+    }
+
+
+}
