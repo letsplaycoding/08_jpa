452f57f241205dd8634fa6c89a6bfd45f8fa5f27
alu98@naver.com <alu98@naver.com>
2024-02-28 12:31:46 +0900

    paging 처리 menuList 조회기능까지 완료

--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -6,6 +6,9 @@ import com.ohgiraffers.springdatajpa.menu.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -45,5 +48,21 @@ public class MenuService {
         return menuList.stream().map(menu -> mapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
     }
 
+    /* 설명. 3. findAll(페이징 처리 후 )*/
+    public Page<MenuDTO> findMenuList(Pageable pageable) {
 
+        /* 설명.
+         *  1.넘어온 pageabe에 담긴 페이지 번호를 인덱스 개념으로 바꿔서 인지 시킴.
+         *  2. 한 페이지에 뿌려질 데이터 크기
+         *  3. 정렬 기준
+        * */
+
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber()-1,
+                pageable.getPageSize(),
+                                    Sort.by("menuCode").descending());
+
+        Page<Menu> menuList = menuRepository.findAll(pageable);
+
+        return menuList.map(menu -> mapper.map(menu,MenuDTO.class));
+    }
 }
